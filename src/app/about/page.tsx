745a0e658627d59ab26466a9906371aad4225b69
imdeepss <deepakk@geeconglobal.com>
2025-01-13 12:38:17 +0530

    Fix: Adding Extra page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@/_components/layouts";
+import React from "react";
+
+const page = () => {
+  return <Container className="py-20"></Container>;
+};
+
+export default page;
