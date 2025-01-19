0c4a2df11304f0bbddb2005b8e07f91388a383e1
DK <deepakk@geeconglobal.com>
2025-01-19 20:50:48 +0530

    Feat: Seo changes for single page

--- src/app/(home)/[slug]/page.tsx
+++ src/app/(home)/[slug]/page.tsx
@@ -1,6 +1,36 @@
 import { getPostBySlug } from "@/sanity/sanity.query";
 import { notFound } from "next/navigation";
 import { ContentSection } from "./_components";
+import type { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  {
+    params,
+  }: Readonly<{
+    params: { slug: string };
+  }>,
+  _parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  const post = await getPostBySlug(slug);
+
+  if (post.seo) {
+    return {
+      title: post.seo.seoTitle,
+      description: post.seo.seoDescription,
+      openGraph: {
+        images: post.seo.seoImage?.assest?._ref,
+      },
+    };
+  }
+
+  return {
+    title: "देव आरती संग्रह | हिन्दू भक्ति गीतों और प्रार्थनाओं का संग्रह",
+    description:
+      "देव आरती संगरह में हिन्दू भक्ति गीतों और आरतियों का संपूर्ण संग्रह पाएं। आध्यात्मिकता, श्रद्धा और दैनिक पूजा से जुड़ी प्रार्थनाओं को खोजें।",
+  };
+}
 
 export default async function Page({
   params,
