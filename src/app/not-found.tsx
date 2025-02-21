7e7964ece6ab4b135587f9419f7cac4b6cb09141
imdeepss <deepakk@geeconglobal.com>
2025-02-21 09:29:44 +0530

    feat: update message for 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
   return (
     <Container className="text-secondary text-center py-20">
       <h2 className="text-3xl underline underline-offset-1 text-secondary font-bold">
-        Not found
+        Coming Soon
       </h2>
-      <p>Could not find requested resource</p>
+      <p>We are working hard to bring you this feature. Stay tuned!</p>
       <Link href="/">Return Home</Link>
     </Container>
   );
